54e206d321e5cffa09015bff89217619f009fd6f
noman2002 <nomn2002@gmail.com>
2021-07-10 18:28:52 +0530

    homepage added

---
+++ lib/article.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Article extends StatefulWidget {
+  const Article({ Key? key }) : super(key: key);
+
+  @override
+  _ArticleState createState() => _ArticleState();
+}
+
+class _ArticleState extends State<Article> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
